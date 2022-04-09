7d8edac2e9ccb829bab24ff85d664d45c826c62f
michaelkustra <100160450+michaelkustra@users.noreply.github.com>
2022-04-09 20:13:06 +0100

    commit additions to ggplot

--- week_05/weekend/shiny_weekend_hw/app.R
+++ week_05/weekend/shiny_weekend_hw/app.R
@@ -3,44 +3,64 @@ library(DT)
 library(tidyverse)
 
 
-
 ui <- fluidPage(
 
     
     titlePanel("🎮👾🔫 MK Games Dashboard 🔫👾🎮"),
 
     
-    sidebarLayout(
-        sidebarPanel(
-          selectInput("developer_input",
-                      "Choose Developer",
-                      choices = unique(games_data$developer)
-            
-          ),
-          
-          selectInput("platform_input",
-                      "Choose Platform",
-                      choices = unique(games_data$platform)
-            
-          ),
-          
-          selectInput("genre_input",
-                      "Choose Genre",
-                      choices = unique(games_data$genre)
-            
-          )
+    fluidPage(
+      fluidRow(
+        column(2,
+               selectInput("developer_input",
+                    "Choose Developer",
+                    choices = unique(games_data$developer))
+                    
         ),
+        
+        column(2,
+               selectInput("platform_input",
+                    "Choose Platform",
+                    choices = unique(games_data$platform))
+                    
+        ),
+        
+        column(2,
+               selectInput("genre_input",
+                    "Choose Genre",
+                    choices = unique(games_data$genre))
+                    
+        ),
+        
+        column(2,
+               selectInput(
+                 "year_input",
+                 "Highlight Year of Release",
+                 choices = unique(games_data$year_of_release)
+               )),
+        
+        column(3, 
+               actionButton("update",
+                     "Refresh Selection"))
+      ),
+         
 
         # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("games_plot")
+        fluidRow(
+           column(6, 
+                  plotOutput("games_plot")),
+      
+      
+          column(6,
+                 plotOutput("cor_plot"))
         )
     )
 )
 
+
 server <- function(input, output) {
   
-  filtered_data <- eventReactive({
+  filtered_data <- eventReactive(input$update, {
     games_data %>% 
       filter(developer == input$developer_input) %>% 
       filter(platform == input$platform_input) %>% 
@@ -49,11 +69,25 @@ server <- function(input, output) {
 
     output$games_plot <- renderPlot({
       
-      filtered_data %>% 
+      games_data %>%
+        mutate(year_select = if_else(
+          year_of_release == input$year_input, "yes", "no")) %>% 
       ggplot() +
-        aes(x = critic_score, y = user_score) +
-        geom_point()
+        aes(x = year_of_release, y = sales) +
+        geom_col(aes(fill = year_select), show.legend = F) +
+        labs(title = "Total Game Sales by Year",
+             x = "Year of Release",
+             y = "Total Sales") +
+        
+        theme_bw()
+    })
+    
+    output$cor_plot <- renderPlot({
       
+      filtered_data() %>% 
+        ggplot() +
+        aes(x = critic_score, y = user_score, fill = input$developer_input) +
+        geom_point()
     })
 }
 
