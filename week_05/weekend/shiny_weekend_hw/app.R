5ec0567a55cf1e2bb56b45b2c8184b000c90decf
michaelkustra <100160450+michaelkustra@users.noreply.github.com>
2022-04-10 19:57:12 +0100

    updated & working shiny app

--- week_05/weekend/shiny_weekend_hw/app.R
+++ week_05/weekend/shiny_weekend_hw/app.R
@@ -4,58 +4,65 @@ library(tidyverse)
 
 
 ui <- fluidPage(
-
+  
+  
+  titlePanel("🎮👾🔫 MK Games Dashboard 🔫👾🎮"),
+  
+  
+  tabsetPanel(
+    tabPanel(
+      "Games Data Table",
+      dataTableOutput("table_output")
+    ),
     
-    titlePanel("🎮👾🔫 MK Games Dashboard 🔫👾🎮"),
-
-    
-    fluidPage(
-      fluidRow(
-        column(2,
-               selectInput("developer_input",
-                    "Choose Developer",
-                    choices = unique(games_data$developer))
-                    
-        ),
-        
-        column(2,
-               selectInput("platform_input",
-                    "Choose Platform",
-                    choices = unique(games_data$platform))
-                    
-        ),
-        
-        column(2,
-               selectInput("genre_input",
-                    "Choose Genre",
-                    choices = unique(games_data$genre))
-                    
-        ),
-        
-        column(2,
-               selectInput(
-                 "year_input",
-                 "Highlight Year of Release",
-                 choices = unique(games_data$year_of_release)
-               )),
-        
-        column(3, 
-               actionButton("update",
-                     "Refresh Selection"))
+    tabPanel(
+      "User Score Filter",
+      plotOutput("user_plot"),
+      
+      column(3, 
+             selectInput("genre_input",
+                         "Select Genre",
+                         choices = unique(games_data$genre))
       ),
-         
-
-        # Show a plot of the generated distribution
-        fluidRow(
-           column(6, 
-                  plotOutput("games_plot")),
+      
+      column(3, 
+             selectInput(
+               "developer_input",
+               "Select Developer",
+               choices = unique(games_data$developer))
+      ),
+      
+      column(3,
+             selectInput("platform_input",
+                         "Select Platform",
+                         choices = unique(games_data$platform))
+      ),
       
       
-          column(6,
-                 plotOutput("cor_plot"))
-        )
+      column(3, 
+             actionButton("update",
+                          "Refresh Plot")
+             
+      )),
+    
+    tabPanel(
+      "Game Sales by Year",
+      plotOutput("games_plot"),
+      selectInput(
+        "year_input",
+        "Highlight Year of Release",
+        choices = unique(games_data$year_of_release)
+      )
+    ),
+    
+    tabPanel(
+      "South Park",
+      tags$a("Gamer gif", href = 
+               "https://giphy.com/gifs/southpark-comedy-central-10x08-xTiTnwgQ8Wjs1sUB4k/tile")
     )
+  )
 )
+
 
 
 server <- function(input, output) {
@@ -65,30 +72,42 @@ server <- function(input, output) {
       filter(developer == input$developer_input) %>% 
       filter(platform == input$platform_input) %>% 
       filter(genre == input$genre_input)
-  })
-
-    output$games_plot <- renderPlot({
-      
-      games_data %>%
-        mutate(year_select = if_else(
-          year_of_release == input$year_input, "yes", "no")) %>% 
-      ggplot() +
-        aes(x = year_of_release, y = sales) +
-        geom_col(aes(fill = year_select), show.legend = F) +
-        labs(title = "Total Game Sales by Year",
-             x = "Year of Release",
-             y = "Total Sales") +
-        
-        theme_bw()
-    })
     
-    output$cor_plot <- renderPlot({
-      
-      filtered_data() %>% 
-        ggplot() +
-        aes(x = critic_score, y = user_score, fill = input$developer_input) +
-        geom_point()
-    })
+  })
+  
+  output$user_plot <- renderPlot({
+    
+    filtered_data() %>% 
+      ggplot() +
+      aes(x = name, y = user_score) +
+      geom_col(colour = "grey10", fill = "pink", show.legend = F) +
+      coord_flip()+
+      labs(title = "User Score Filter",
+           y = "User Score",
+           x = "Game") +
+      theme_bw()
+    
+  })
+  
+  output$table_output <- renderDataTable({
+    
+    games_data
+    
+  })
+  
+  output$games_plot <- renderPlot({
+    
+    games_data %>%
+      mutate(year_select = if_else(
+        year_of_release == input$year_input, "yes", "no")) %>% 
+      ggplot() +
+      aes(x = year_of_release, y = sales) +
+      geom_col(aes(fill = year_select), show.legend = F) +
+      labs(title = "Total Game Sales by Year",
+           x = "Year of Release",
+           y = "Total Sales") +
+      theme_bw()
+  })
 }
 
 shinyApp(ui = ui, server = server)
